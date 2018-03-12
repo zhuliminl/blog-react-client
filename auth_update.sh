bd1bae0eab5cdd5e3f09608688ead108a48c36b6
xiaoshitou <zhuliminl@gmail.com>
2018-03-12 15:53:03 +0800

    后续优化更新

--- auth_update.sh
+++ auth_update.sh
@@ -3,13 +3,13 @@ npm run build
 git add .
 git commit -m '后续优化更新'
 git push -u origin master
-
 echo '前端已经更新到远程仓库'
 
 cd ../server/
 git add .
 git commit -m '后续优化更新'
 git push -u origin master
+echo '后端已经更新到远程仓库'
 
 cd ../client/
 
